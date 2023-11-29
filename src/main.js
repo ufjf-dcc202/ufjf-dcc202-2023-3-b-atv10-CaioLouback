c6f36e200cf566c7d5741ab0f21f49fe97e433d8
Caio Louback <caio.louback@estudante.ufjf.br>
2023-11-29 11:04:16 -0300

    atv10 feita em casa

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import {getJoao, getMaria} from "../joaoEMaria.js";
+
+
+
+console.log('módulo main');
+
+console.log('João tem ', getJoao(), 'maçãs!');
+console.log('Maria tem ', getMaria(), 'maçãs!');
